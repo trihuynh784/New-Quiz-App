eb12f8d2e29ebbcb411a90e75fcbea2d0f7410bd
Huỳnh Minh Trí <mtrigenius@gmail.com>
2025-03-09 22:23:50 +0700

    Update API_DOMAIN

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -1,4 +1,5 @@
-const API_DOMAIN = "http://localhost:3002/";
+// const API_DOMAIN = "http://localhost:3002/";
+const API_DOMAIN = "https://quiz-db.vercel.app/";
 
 export const get = async (path) => {
   const response = await fetch(API_DOMAIN + path);
